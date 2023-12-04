0c49755957cd5625446bdbbc062dd395b1d3a168
Lex9jar <alexanderedim89@gmail.com>
2023-12-04 15:07:43 +0100

    Selects a specific function for a specifier

---
+++ sought_for_function.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * fmt_n_print - Sought for a function for a particular specifier.
+ * @format: Format string to get specifier.
+ * @spc_idx: Index number of specifier in format.
+ * @arg_ptr: Argument pointer.
+ *
+ * Return: printed number of characters from a function.
+ */
+int fmt_n_print(const char *format, int spc_idx, va_list arg_ptr)
+{
+	store fetch[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'\0', NULL}
+	};
+	int i = 0;
+
+	while (fetch[i].c)
+		if (fetch[i].c == format[spc_idx])
+			return (fetch[i].func(arg_ptr));
+
+	return (-1);
+}
