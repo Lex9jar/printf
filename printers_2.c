91a662f2da0dc7deb3c3df9f6a9750fda1dbf5dc
Lex9jar <alexanderedim89@gmail.com>
2023-12-06 02:29:30 +0100

    Added a function to print a string in ROT13 format

--- printers_2.c
+++ printers_2.c
@@ -76,3 +76,70 @@ int print_pointer(va_list arg_ptr, char buffer[])
 		count += _putchar(buffer[j]);
 	return (count);
 }
+
+/**
+ * print_reverse - Prints a string in reverse
+ * @arg_ptr: Argument pointer
+ * @buffer: Buffer for formatting and printing
+ *
+ * Return: Number of characters printed.
+ */
+int print_reverse(va_list arg_ptr, char buffer[])
+{
+	char *str;
+	int i, j = 0, count = 0;
+
+	str = va_arg(arg_ptr, char *);
+	if (str == NULL)
+		return (write(1, ")llun(", 6));
+
+	buffer[BUFFER_SIZE - 1] = '\0';
+	i = BUFFER_SIZE - 2;
+
+	while (str[j])
+		buffer[i--] = str[j++];
+	i++;
+
+	for (j = i; buffer[j]; j++)
+		count += _putchar(buffer[j]);
+	return (count);
+}
+
+/**
+ * print_in_ROT13 - Prints a string in ROT13
+ * @arg_ptr: Argument pointer
+ * @buffer: Buffer for formatting and printing
+ *
+ * Return: Number of characters printed.
+ */
+int print_in_ROT13(va_list arg_ptr, char buffer[])
+{
+	char *up = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *dw = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *str;
+	int i, j, idx = 0, count = 0;
+
+	str = va_arg(arg_ptr, char *);
+	if (str == NULL)
+		return (write(1, "(AHYY)", 6));
+
+	for (i = 0; str[i]; i++, j++)
+	{
+		for (j = 0; up[j]; j++)
+		{
+			if (str[i] == up[j])
+			{
+				buffer[idx++] = dw[j];
+				break;
+			}
+		}
+		if (up[j] == '\0')
+			buffer[idx++] = str[i];
+	}
+	buffer[idx] = '\0';
+
+	for (i = 0; buffer[i]; i++)
+		count += _putchar(buffer[i]);
+
+	return (count);
+}
