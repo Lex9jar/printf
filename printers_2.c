b1c1bc9eb0dc7a1700f8af0ae0ac65fb18a87dee
Lex9jar <alexanderedim89@gmail.com>
2023-12-06 01:32:07 +0100

    Added a function for printing printers

--- printers_2.c
+++ printers_2.c
@@ -41,3 +41,38 @@ int print_String(va_list arg_ptr, char buffer[])
 		count += _putchar(buffer[i]);
 	return (count);
 }
+
+/**
+ * print_pointer - Prints the address of a memory
+ * @arg_ptr: Argument pointer
+ * @buffer: Buffer for formatting and printing
+ *
+ * Return: Number of characters printed.
+ */
+int print_pointer(va_list arg_ptr, char buffer[])
+{
+	void *ptr;
+	char hex_arr[] = "0123456789abcdef";
+	unsigned long num_ptr;
+	int i, j, count = 0;
+
+	ptr = va_arg(arg_ptr, void *);
+	if (ptr == NULL)
+		return (write(1, "(nil)", 5));
+
+	buffer[BUFFER_SIZE + 1] = '\0';
+	i = BUFFER_SIZE - 2;
+
+	num_ptr = (unsigned long)ptr;
+	while (num_ptr > 0)
+	{
+		buffer[i--] = hex_arr[num_ptr % 16];
+		num_ptr /= 16;
+	}
+	buffer[i--] = 'x';
+	buffer[i] = '0';
+
+	for (j = i; buffer[j]; j++)
+		count += _putchar(buffer[j]);
+	return (count);
+}
